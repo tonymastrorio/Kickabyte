817e65bb52a242638f7b21f43b0990897a4a8a37
Tony <github email address>
2018-03-06 21:50:52 -0500

    test different settings in production.rb for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,18 +60,20 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "gramps_#{Rails.env}"
-  config.action_mailer.default_url_options = { :host => 'https://www.kickabyte.com' }
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'https://www.kickabyte.com',
-    user_name:            'tmastrorio@gmail.com',
-    password:             'dctkkmiqlavwshbr',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { :host => 'https://www.kickabyte.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "https://www.kickabyte.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'tmastrorio@gmail.com',
+    password: 'dctkkmiqlavwshbr',
+}
 
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.gmail.com',
@@ -84,7 +86,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
